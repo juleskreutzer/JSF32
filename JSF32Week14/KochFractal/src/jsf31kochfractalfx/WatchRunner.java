b15bf824706773d05de3eefbc7966ce463fb614e
Jules Kreutzer <juleskreutzer@me.com>
2015-12-09 18:12:30 +0100

    Finished assignment week 14

---
+++ JSF32Week14/KochFractal/src/jsf31kochfractalfx/WatchRunner.java
@@ -0,0 +1,198 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsf31kochfractalfx;
+
+import calculate.Edge;
+import static com.sun.jmx.mbeanserver.Util.cast;
+import java.nio.file.*;
+import static java.nio.file.StandardWatchEventKinds.*;
+import static java.nio.file.LinkOption.*;
+import java.nio.file.attribute.*;
+import java.io.*;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.scene.paint.Color;
+
+/**
+ *
+ * @author jules
+ */
+public class WatchRunner implements Runnable {
+
+    
+    private final WatchService watcher;  // the service object who processes events for us
+    private final Map<WatchKey, Path> keys; // the map of WatchKey's and belonging Path
+    private final boolean recursive;
+    private boolean trace = false;
+    private JSF31KochFractalFX fx;
+    
+    private ArrayList<Edge> edges;
+    
+    private Path dir;
+    
+    public WatchRunner(Path dir, boolean recursive, JSF31KochFractalFX fx) throws IOException
+    {
+        this.watcher = FileSystems.getDefault().newWatchService();
+        this.keys = new HashMap<WatchKey, Path>();  // map of watchkeys and path belonging to it
+        this.recursive = recursive;
+        this.dir = dir;
+        this.fx = fx;
+        
+        edges = new ArrayList<>();
+
+        // register all Paths to watch
+        if (recursive) {
+            System.out.format("Scanning %s ...\n", dir);
+            registerAll(dir);
+            System.out.println("Done.");
+        } else {
+            register(dir);
+        }
+    }
+    
+    /**
+     * Register the given directory with the WatchService First, create a
+     * WatchKey object which connects all event types to a certain path, and add
+     * it to the WatchService. keep an own Map for tracing
+     *
+     */
+    private void register(Path dir) throws IOException {
+
+        WatchKey key = dir.register(this.watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+        if (trace) {
+            Path prev = keys.get(key);
+            if (prev == null) {
+                System.out.format("register: %s\n", dir);
+            } else {
+                if (!dir.equals(prev)) {
+                    System.out.format("update: %s -> %s\n", prev, dir);
+                }
+            }
+        }
+        keys.put(key, dir);
+    }
+
+    /**
+     * Register the given directory, and all its sub-directories, with the
+     * WatchService.
+     */
+    private void registerAll(final Path start) throws IOException {
+        // register directory and sub-directories
+        Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
+                    throws IOException {
+                register(dir);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+    
+    public ArrayList<Edge> getEdges()
+    {
+        return this.edges;
+    }
+    
+    @Override
+    public void run() {
+        for (;;) {
+
+            // wait for key to be signalled
+            WatchKey key;
+            try {
+                key = watcher.take();
+            } catch (InterruptedException x) {
+                return;
+            }
+
+            Path dir = keys.get(key);
+            if (dir == null) {
+                System.err.println("WatchKey not recognized!!");
+                continue;
+            }
+
+            for (WatchEvent<?> event : key.pollEvents()) {
+                // get event kind
+                WatchEvent.Kind kind = event.kind();
+
+                // TBD - provide example of how OVERFLOW event is handled
+                if (kind == OVERFLOW) {
+                    continue;  // something unexpected happened, let's ignore this
+                }
+
+                // Context for directory entry event is the file name of entry
+                WatchEvent<Path> ev = cast(event);
+                Path filename = ev.context();
+                Path child = dir.resolve(filename);
+
+                // or you could use: 
+                // Path filename = (Path) event.context();
+                // this leads to the same result as ev.context() below
+                
+                
+                // print out event
+                System.out.format("%s: %s\n", event.kind().name(), child);
+                
+                if(filename.toString().equals("MappedResultWritten.txt") && event.kind().name().equals("ENTRY_CREATE"))
+                {
+                    try {
+                        RandomAccessFile file = new RandomAccessFile(new File("/media/jules/secondDisk/JSF32/week13/MappedResultWritten.txt"), "r");
+                        
+                        FileChannel fc = file.getChannel();
+                        MappedByteBuffer out = fc.map(FileChannel.MapMode.READ_ONLY, 0, file.length());
+                        //long length = file.length();
+                        while(out.hasRemaining())
+                        {
+                            double X1 = out.getDouble();
+                            double Y1 = out.getDouble();
+                            double X2 = out.getDouble();
+                            double Y2 = out.getDouble();
+                            
+                            Color color = Color.color(out.getDouble(), out.getDouble(), out.getDouble());
+                            
+                            edges.add(new Edge(X1, Y1, X2, Y2, color));
+                        }
+                        
+                        fx.startDrawing(edges);
+                        
+                    } catch (FileNotFoundException ex) {
+                        Logger.getLogger(WatchRunner.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (IOException ex) {
+                        Logger.getLogger(WatchRunner.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    
+                }
+
+                // if directory is created, and watching recursively, then
+                // register it and its sub-directories
+                if (recursive && (kind == ENTRY_CREATE)) {
+                    try {
+                        if (Files.isDirectory(child, NOFOLLOW_LINKS)) {
+                            registerAll(child);
+                        }
+                    } catch (IOException x) {
+                        // ignore to keep sample readable
+                    }
+                }
+            }
+
+            // reset key (because you just handled it, and remove from set if directory no longer accessible
+            boolean valid = key.reset();
+            if (!valid) {
+                keys.remove(key);
+
+                // all directories are inaccessible
+                if (keys.isEmpty()) {
+                    break;
+                }
+            }
+        }
+    }
+    
+}
---
+++ JSF32Week14/KochFractal/src/jsf31kochfractalfx/Writer.java
@@ -0,0 +1,176 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsf31kochfractalfx;
+
+import calculate.Edge;
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ *
+ * @author jules
+ */
+public class Writer implements Observer {
+    private int totalEdges;
+    private List<Edge> edges;
+    private int level; 
+    
+    public Writer(int level)
+    {
+        this.level = level;
+        KochFractal koch = new KochFractal();
+        koch.addObserver(this);
+        koch.setLevel(level);
+        
+        //totalEdges = koch.getNrOfEdges();
+        edges = new ArrayList<>();
+        
+        koch.generateBottomEdge();
+        koch.generateLeftEdge();
+        koch.generateRightEdge();
+        
+        totalEdges = koch.getNrOfEdges();
+        if(totalEdges >= edges.size())
+        {
+        writeToFile();
+        }
+    }
+            
+
+    @Override
+    public void update(Observable o, Object arg) {
+        edges.add((Edge)arg);
+        
+    }
+    
+    private void writeToFile()
+    {
+        DataOutputStream dos = null;
+        
+        try{
+            
+            File file = new File("/media/jules/secondDisk/JSF32/week13/MappedResult.txt");
+            File file2 = new File("/media/jules/secondDisk/JSF32/week13/MappedResultWritten.txt");
+            
+            //Delete the file; we will create a new file
+            file.delete();
+
+            // Get file channel in readonly mode
+            FileChannel fileChannel = new RandomAccessFile(file, "rw").getChannel();
+
+            // Get direct byte buffer access using channel.map() operation
+            // 7 values * 8 bytes(double = 8byte) 
+            MappedByteBuffer buffer = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, (7*8)*edges.size());
+            //long length = (7*8)*edges.size();
+            // fileChannel.map(Mode, position, size)
+            // http://docs.oracle.com/javase/7/docs/api/java/nio/channels/FileChannel.html#map(java.nio.channels.FileChannel.MapMode,%20long,%20long)
+
+            //Write the content using put methods
+            for(Edge e : edges){
+                buffer.putDouble(e.X1);
+                buffer.putDouble(e.Y1);
+                buffer.putDouble(e.X2);
+                buffer.putDouble(e.Y2);
+                buffer.putDouble(e.color.getRed());
+                buffer.putDouble(e.color.getGreen());
+                buffer.putDouble(e.color.getBlue());
+            }
+            
+            if(file.exists())
+            {
+                file.renameTo(file2);
+            }
+            
+            System.out.print("Result to MappedResult.txt done!");
+            
+            // Write text
+            dos = new DataOutputStream(new FileOutputStream("/media/jules/secondDisk/JSF32/week12/result.txt"));
+            dos.writeInt(level);
+            for(Edge e : edges)
+            {
+                dos.writeDouble(e.X1);
+                dos.writeDouble(e.Y1);
+                dos.writeDouble(e.X2);
+                dos.writeDouble(e.Y2);
+                dos.writeDouble(e.color.getRed());
+                dos.writeDouble(e.color.getGreen());
+                dos.writeDouble(e.color.getBlue());
+            }
+            dos.close();
+            System.out.print("DataOutputStream to result.txt done!\n");
+            
+            
+            // Write text with buffer
+            dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("/media/jules/secondDisk/JSF32/week12/result_buffer.txt")));
+            dos.writeInt(level);
+            for(Edge e : edges)
+            {
+                dos.writeDouble(e.X1);
+                dos.writeDouble(e.Y1);
+                dos.writeDouble(e.X2);
+                dos.writeDouble(e.Y2);
+                dos.writeDouble(e.color.getRed());
+                dos.writeDouble(e.color.getGreen());
+                dos.writeDouble(e.color.getBlue());
+            }
+            dos.close();
+            System.out.print("DataOutputStream to result_buffer.txt done!\n");
+            
+            // Write to bin file
+            dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("/media/jules/secondDisk/JSF32/week12/result_buffer.bin")));
+            dos.writeInt(level);
+            for(Edge e : edges)
+            {
+                dos.writeDouble(e.X1);
+                dos.writeDouble(e.Y1);
+                dos.writeDouble(e.X2);
+                dos.writeDouble(e.Y2);
+                dos.writeDouble(e.color.getRed());
+                dos.writeDouble(e.color.getGreen());
+                dos.writeDouble(e.color.getBlue());
+            }
+            dos.close();
+            System.out.print("DataOutputStream to result_buffer.bin done!\n");
+            
+            dos = new DataOutputStream(new FileOutputStream("/media/jules/secondDisk/JSF32/week12/result.bin"));
+            dos.writeInt(level);
+            for(Edge e : edges)
+            {
+                dos.writeDouble(e.X1);
+                dos.writeDouble(e.Y1);
+                dos.writeDouble(e.X2);
+                dos.writeDouble(e.Y2);
+                dos.writeDouble(e.color.getRed());
+                dos.writeDouble(e.color.getGreen());
+                dos.writeDouble(e.color.getBlue());
+            }
+           dos.close();
+           System.out.print("DataOutputStream to result.bin done!\n");
+            
+        } catch(FileNotFoundException ex)
+        {
+            System.out.print(ex.getMessage());
+        }
+        catch(IOException ex)
+        {
+            System.out.print(ex.getMessage());
+        }
+        
+    }
+    
+}
